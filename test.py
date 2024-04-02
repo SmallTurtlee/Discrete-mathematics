894008d85e6fdc77e518da8ce6f40eb11da83eda
SmallTurtlee <na.sem.2007@yandex.ru>
2024-04-02 20:59:52 +0300

    Добавлен файл с ответом №1

--- test.py
+++ test.py
@@ -8,12 +8,14 @@ def create_function():
     oper =  ['\\vee', '\\wedge', '\\oplus', '\\rightarrow', '\\mid', '\\downarrow', '\\sim']
     return '[(' + perem[0] +' '+ random.choice(oper) +' '+ perem[1]+') ' + random.choice(oper) + ' (' + perem[2] +' '+ random.choice(oper) +' '+ perem[3] +')] ' + random.choice(oper) + ' ' + perem[4] + '$'
     
-def text_of_task_1():
+    
+def text_of_task_1(i):
     task1 = '$f(x_1, x_2, x_3) = '
     f = create_function()
     while f.find('x_1') == -1 or f.find('x_2') == -1 or f.find('x_3') == -1:
         f = create_function()
     task1 += f 
+    get_first_answer(f, i)
     return task1
 
 def text_of_task_2():
@@ -26,8 +28,9 @@ def text_of_task_2():
     return task2
 
 def formula(i, nn):
-    if nn==1: tex = text_of_task_1()
-    else: tex = text_of_task_2()
+    if nn==1: tex = text_of_task_1(i)
+    elif nn==2: tex = text_of_task_2()
+    else: tex = nn
     fig = plt.figure()
     ax = fig.add_axes([0,0,1,1])
     ax.set_axis_off()
@@ -41,25 +44,94 @@ def formula(i, nn):
     plt.savefig('test'+str(i)+'.png', dpi=300)
     plt.close(fig)
 
+def get_first_answer(text, i):
+    list =[]
+    list.append(['x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6', 'f'])
+    list.append([0,0,0,0,1,1,1,1])
+    list.append([0,0,1,1,0,0,1,1])
+    list.append([0,1,0,1,0,1,0,1])
+    word = text.split()
+    list.append(getlogic(word[0], word[1], word[2], list))
+    list.append(getlogic(word[4],word[5], word[6], list))
+    list.append(getlogic("x_4", word[3], "x_5", list))
+    list.append(getlogic("x_6", word[7], word[8], list))
+    answtex='1.' +str(list[-1][0])+str(list[-1][1])+str(list[-1][2])+str(list[-1][3])+str(list[-1][4])+str(list[-1][5])+str(list[-1][6])+str(list[-1][7])
+    pdfanswer.cell(0, 5, answtex , ln=1)
+    list_of_dis= ['\\overline{x_1} \\overline{x_2} \\overline{x_3}', '\\overline{x_1} \\overline{x_2} x_3', '\\overline{x_1} x_2 \\overline{x_3}', '\\overline{x_1} x_2 x_3', 'x_1 \\overline{x_2} \\overline{x_3}', 'x_1 \\overline{x_2} x_3', 'x_1 x_2 \\overline{x_3}', 'x_1 x_2 x_3']
+    answdnf='$'
+    list_of_kon=['(\\overline{x_1} \\vee \\overline{x_2} \\vee \\overline{x_3})', '(\\overline{x_1} \\vee \\overline{x_2} \\vee x_3)', '(\\overline{x_1} \\vee x_2 \\vee \\overline{x_3})', '(\\overline{x_1} \\vee x_2  \\vee x_3)', '(x_1 \\vee \\overline{x_2} \\vee \\overline{x_3})', '(x_1 \\vee \\overline{x_2}  \\vee x_3)', '(x_1 \\vee x_2 \\vee \\overline{x_3})', '(x_1 \\vee x_2 \\vee x_3)']
+    answknf='$'
+    for j in range(8):
+        if list[-1][j] == 1: answdnf += list_of_dis[j] + '\\vee '
+        else: answknf += list_of_kon[j] + ' '
+    pdfanswer.cell(0,5,"СДНФ:", ln=1)
+    formula(i+2,answdnf[:-5]+'$')
+    pdfanswer.image("test"+str(i+2)+".png")
+    pdfanswer.cell(0,5,"СКНФ:", ln=1)
+    formula(i+3, answknf+'$')
+    pdfanswer.image("test"+str(i+3)+".png")
+    
+def getlogic(n, oper, m, list):
+    x=[]
+    y=[]
+    k = int(n[n.find('x')+2])
+    l = int(m[m.find('x')+2])
+    for i in range(8):
+        if n.find('overline') == -1: x.append(list[k][i]) 
+        else: x.append(int(not(list[k][i])))
+        if m.find('overline') == -1: y.append(list[l][i])
+        else: y.append(int(not(list[l][i])))
+    return(dologic(x, y, oper))
+    
+def dologic(x, y, oper):
+    a = []
+    if oper.find('vee') != -1: 
+        for i in range(8):
+            a.append(int(bool(x[i]) or bool(y[i])))
+    elif oper.find('wedge') != -1: 
+        for i in range(8):
+            a.append(int(bool(x[i]) and bool(y[i])))
+    elif oper.find('oplus') != -1: 
+        for i in range(8):
+            a.append(int(not(bool(x[i])) and bool(y[i]) or bool(x[i]) and not(bool(y[i]))))
+    elif oper.find('rightarrow') != -1: 
+        for i in range(8):
+            a.append(int(not(bool(x[i])) or bool(y[i])))
+    elif oper.find('sim') != -1: 
+        for i in range(8):
+            a.append(int(bool(x[i]) == bool(y[i])))
+    elif oper.find('mid') != -1: 
+        for i in range(8):
+            a.append(int(not(bool(x[i]) and bool(y[i]))))
+    elif oper.find('downarrow') != -1: 
+        for i in range(8):
+            a.append(int(not(bool(x[i]) or bool(y[i]))))
+    return a
 
 pdf = FPDF()
+pdfanswer = FPDF()
 pdf.add_font('times', '', 'timesnrcyrmt.ttf', uni=True)
 pdf.add_font('timesB', '', 'timesnrcyrmt_bold.ttf', uni=True)
+pdfanswer.add_font('times', '', 'timesnrcyrmt.ttf', uni=True)
 pdf.add_page()
+pdfanswer.add_page()
+pdfanswer.set_font("times", size=12)
 print("Сколько вариантов сгенерировать?")
 num = int(input())
 for i in range(num):
+    pdfanswer.cell(0, 5, "Вариант №"+str(i+1), ln=1)
     pdf.set_font("timesB", size=12)
     pdf.cell(0, 5,'Дискретная математика. РК1 "Теория булевых функций".           Вариант №' + str(i+1),ln=1, align="L")
     pdf.set_font("times", size=12)
     pdf.cell(0, 5, '1. Используя соответствующую методику, аналитически привести заданную булеву функцию к ' + random.choice(['Д', 'К']) + 'НФ.', ln=1)
-    formula(i*2, 1)
-    pdf.image("test"+str(i*2)+".png")
+    formula(i*4, 1)
+    pdf.image("test"+str(i*4)+".png")
     pdf.cell(0, 5, '2. Построить таблицу истинности и записать СДНФ заданной булевой функции. Получить ', ln=1)
     pdf.cell(0, 5, random.choice(['все её минимальные', 'её минимальную']) + ' ДНФ методом карт Карно.', ln=1)
-    formula(i*2+1, 2)
-    pdf.image("test"+str(i*2+1)+".png")
+    formula(i*4+1, 2)
+    pdf.image("test"+str(i*4+1)+".png")
     if i % 5 != 4: pdf.cell(0, 15, "_"*1000, ln=1, align="C")
     else: pdf.cell(0, 5, " " * 1000, ln=1)
 
 pdf.output("demo_rk1.pdf")
+pdfanswer.output("answers.pdf")
